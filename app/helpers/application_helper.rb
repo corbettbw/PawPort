0222ee06c04052a986f4532c29da369a2a5f44d7
Corbett Winningham <corbettbw@gmail.com>
2025-10-15 16:53:14 -0700

    adding styling and alert messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def flash_class(type)
+        case type.to_s
+        when "notice" then "bg-green-50 border border-green-300 text-green-800"
+        when "alert", "error" then "bg-red-50 border border-red-300 text-red-800"
+        else "bg-gray-50 border border-gray-300 text-gray-800"
+    end
+  end
 end
