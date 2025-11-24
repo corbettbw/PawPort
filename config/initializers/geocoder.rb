8c844a6e2170528c579a3a721ee1a3a22869e842
Corbett Winningham <corbettbw@gmail.com>
2025-11-24 15:59:36 -0700

    fiddling with the geocoder configs

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,16 +1,10 @@
 # config/initializers/geocoder.rb
-Geocoder.configure do |config|
-  config.timeout = 3          # seconds
-  config.units   = :mi        # or :km
 
-  if Rails.env.production?
-    # Production: real Google calls
-    config.lookup = :google
-    config.api_key = ENV["GOOGLE_MAPS_API_KEY"]
-    config.use_https = true
-  else
-    # Dev & test: NO real HTTP calls by default
-    # :test lookup always returns a fixed fake result and never hits an API.
-    config.lookup = :test
-  end
-end
+Geocoder.configure(
+  timeout: 3,
+  units: :mi,
+
+  lookup: Rails.env.production? ? :google : :test,
+  api_key: Rails.env.production? ? ENV["GOOGLE_MAPS_API_KEY"] : nil,
+  use_https: Rails.env.production?
+)
